fd9903f8a51456b98c7ba1a7116796bdd4998bc7
peterk <peterk@livingwork.com>
2023-03-09 15:09:57 -0500

    some files to jlib base

--- rakefile.rb
+++ rakefile.rb
@@ -1,23 +1,24 @@
 myDir = File.dirname(File.expand_path(__FILE__));
 require "#{myDir}/build-options.rb";
+require "rakish/GitModule";
 
 module Rakish
 
+dependsList = [];
+
   #  cfg = BuildConfig("root");
+
+    dependsList << './artd-lib-logger';
+    dependsList << './artd-jlib-base';
 
 Rakish.Project(
     :includes    => [Rakish::GitModule],
 	:name 		 => "artdlib-cpp",
-    :dependsUpon => [
-        './artd-lib-logger'
-    ],
-    :dependsUponOpt => [
-	    './artd-jlib-base'
-    ]
+    :dependsUpon => dependsList
 ) do
 
     export task :setup => [] do
-    	git.clone("git.livingwork.com:/home/artd/newartd/artd-jlib-base", "#{projectDir}/artd-jlib-base");
+    	git.clone("git.livingwork.com:/home/artd/newartd/artd-jlib-base", "#{myDir}/artd-jlib-base");
     end
 
 end
