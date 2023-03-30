9674b133f891488815d128373f514cc479a3f907
peterk <peterk@livingwork.com>
2023-03-30 13:40:47 -0400

    change rakish to use github repo

--- rakefile.rb
+++ rakefile.rb
@@ -22,9 +22,9 @@ Rakish.Project(
 
     export task :setup => [] do
  	    git.clone("https://github.com/peterkennard/artd-lib-logger.git", "#{projectDir}/artd-lib-logger");
-    	git.clone("git.livingwork.com:/home/artd/newartd/artd-jlib-base", "#{projectDir}/artd-jlib-base");
-    	git.clone("git.livingwork.com:/home/artd/newartd/artd-jlib-thread", "#{projectDir}/artd-jlib-thread");
-    	git.clone("git.livingwork.com:/home/artd/newartd/artd-lib-vecmath", "#{projectDir}/artd-lib-vecmath");
+    	git.clone("https://github.com/peterkennard/artd-jlib-base", "#{projectDir}/artd-jlib-base");
+    	git.clone("https://github.com/peterkennard/artd-jlib-thread.git", "#{projectDir}/artd-jlib-thread");
+    	git.clone("https://github.com/peterkennard/artd-lib-vecmath.git", "#{projectDir}/artd-lib-vecmath");
     end
 
 end
