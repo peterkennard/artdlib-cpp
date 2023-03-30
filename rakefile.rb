dac6182d3bb9fd442ff63f18cdc516bc4871b1e6
peterk <peterk@livingwork.com>
2023-03-30 13:17:07 -0400

    prep for github

--- rakefile.rb
+++ rakefile.rb
@@ -8,10 +8,10 @@ dependsList = [];
 
   #  cfg = BuildConfig("root");
 
-    dependsList << './artd-lib-logger';
-    dependsList << './artd-lib-vecmath';
-    dependsList << './artd-jlib-base';
-    dependsList << './artd-jlib-thread';
+   dependsList << './artd-lib-logger';
+   dependsList << './artd-lib-vecmath';
+   dependsList << './artd-jlib-base';
+   dependsList << './artd-jlib-thread';
 
 
 Rakish.Project(
@@ -21,6 +21,7 @@ Rakish.Project(
 ) do
 
     export task :setup => [] do
+ 	    git.clone("https://github.com/peterkennard/artd-lib-logger.git", "#{projectDir}/artd-lib-logger");
     	git.clone("git.livingwork.com:/home/artd/newartd/artd-jlib-base", "#{projectDir}/artd-jlib-base");
     	git.clone("git.livingwork.com:/home/artd/newartd/artd-jlib-thread", "#{projectDir}/artd-jlib-thread");
     	git.clone("git.livingwork.com:/home/artd/newartd/artd-lib-vecmath", "#{projectDir}/artd-lib-vecmath");
